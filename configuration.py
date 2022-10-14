748ca64694c505be88a511aae6d3728cd9ed8e88
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-10-14 09:46:41 +0900

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -108,7 +108,7 @@ TRAIN_TRANSFER              = True
 TRAIN_LR_INIT               = 1e-4
 TRAIN_LR_END                = 1e-6
 TRAIN_WARMUP_EPOCHS         = 1
-TRAIN_EPOCHS                = 3
+TRAIN_EPOCHS                = 4
 
 # TEST options
 TEST_BATCH_SIZE             = 16
