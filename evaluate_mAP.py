b14edc010f58a2ad3caade6274b3cbfd1e3aebf2
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-10-20 09:34:57 +0900

    Update evaluate_mAP.py

--- evaluate_mAP.py
+++ evaluate_mAP.py
@@ -1,5 +1,7 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 import cv2
 import numpy as np
 import tensorflow as tf
